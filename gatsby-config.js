b5f8d0b6b4b72290d1d3dd1de6416689c426d306
LeKoArts <lekoarts@gmail.com>
2018-03-27 11:56:01 +0200

    Switch from JS to JSX

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-typography',
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
+        pathToConfigModule: 'src/utils/typography.jsx',
       },
     },
   ],
